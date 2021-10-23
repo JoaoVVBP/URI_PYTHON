7a451a24a9f0e8633b8a356a8ef23f7f38a69f3e
João Paiva <joao.vitor.vbp@gmail.com>
2021-10-23 00:23:06 -0300

    Resolução URI_1238 (Otimizaçao Mínima)

--- Strings/URI_1238.py
+++ Strings/URI_1238.py
@@ -1,15 +1,15 @@
 # LANGUAGE	TIME	DATE
-# Mine: Python 3	0.244	10/22/21, 11:07:58 PM
+# Mine: Python 3	0.216	10/22/21, 11:53:16 PM
 # Best: Python	0.048	2/9/18, 4:36:36 PM
+range_value = int(input())
 
 range_value = int(input())
 
 for _ in range(range_value):
-    output_list = []
+    output_string = ""
     input_string = input().split()
     big_string = ""
     small_string = ""
-
 
     if len(input_string[0]) != len(input_string[1]):
         if len(input_string[0]) > len(input_string[1]):
@@ -22,14 +22,14 @@ for _ in range(range_value):
 
         for i in range(len(small_string)):
             if input_string[0] == big_string:
-                output_list += big_string[i] + small_string[i]
+                output_string += big_string[i] + small_string[i]
             elif input_string[1] == big_string:
-                output_list += small_string[i] + big_string[i]
+                output_string += small_string[i] + big_string[i]
 
-        output_list += big_string[len(small_string):]
-        print("".join(output_list))
+        output_string += big_string[len(small_string):]
+        print(output_string)
 
-    elif len(input_string[0]) == len(input_string[1]):
+    else:
         for i in range(len(input_string[0])):
-            output_list += input_string[0][i] + input_string[1][i]
-        print("".join(output_list))
+            output_string += input_string[0][i] + input_string[1][i]
+        print(output_string)
