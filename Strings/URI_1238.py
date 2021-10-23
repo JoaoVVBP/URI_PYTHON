eb8f90c695e7a5c285ab2d18474fa6f0f3e9bf4d
João Paiva <joao.vitor.vbp@gmail.com>
2021-10-23 01:08:45 -0300

    Resolução URI_1238 (Otimizaçao Mínima)

--- Strings/URI_1238.py
+++ Strings/URI_1238.py
@@ -1,33 +1,16 @@
 # LANGUAGE	TIME	DATE
-# Mine: Python 3	0.216	10/22/21, 11:53:16 PM
+# Mine: Python 3.9	0.193	10/23/21, 12:58:51 AM
 # Best: Python	0.048	2/9/18, 4:36:36 PM
 range_value = int(input())
 
 for _ in range(range_value):
     output_string = ""
     input_string = input().split()
-    big_string = ""
-    small_string = ""
 
-    if len(input_string[0]) != len(input_string[1]):
-        if len(input_string[0]) > len(input_string[1]):
-            big_string = input_string[0]
-            small_string = input_string[1]
+    k = 0
+    for i, j in zip(input_string[0], input_string[1]):
+        output_string = output_string + i + j
+        k += 1
+    output_string = output_string + input_string[0][k:] + input_string[1][k:]
+    print(output_string)
 
-        elif len(input_string[0]) < len(input_string[1]):
-            big_string = input_string[1]
-            small_string = input_string[0]
-
-        for i in range(len(small_string)):
-            if input_string[0] == big_string:
-                output_string += big_string[i] + small_string[i]
-            elif input_string[1] == big_string:
-                output_string += small_string[i] + big_string[i]
-
-        output_string += big_string[len(small_string):]
-        print(output_string)
-
-    else:
-        for i in range(len(input_string[0])):
-            output_string += input_string[0][i] + input_string[1][i]
-        print(output_string)
